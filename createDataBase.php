48e0a1b9d98311923885106f8baf8a8b76959b99
Catalan <thicksky@126.com>
2017-10-31 19:56:44 +0800

    实现注册功能

--- createDataBase.php
+++ createDataBase.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<title>Sign up</title>
-	<link rel='stylesheet' type='text/css' href='webstyle.css'>
+	<link rel='stylesheet' type='text/css' href='webstyle1.css'>
 	<style>
 	input {
 		float:left;
@@ -46,35 +46,27 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
 								       PRIMARY                               KEY(id),
 								       UserName                       varchar(15),
 								       Password                         varchar(15),
-								       RealName                       varchar(15),
-								       Email                               varchar(15),
+								       RealName                       varchar(25),
+								       Email                               varchar(25),
 								       QQ                                  varchar(15),
 								       Hometown                     varchar(20),
 								       Signature                        varchar(50)) default charset=utf8 ";
  		if(!mysql_query($command,$con)){
 			;
 		}
-		$command="INSERT INTO User(UserName,Password,RealName,Email,QQ,Hometown,Signature)
-		                                       VALUES('$_POST[UserName]','$_POST[Password]','$Name','$Email','$QQ','$Hometown','$Signature' )";
-		if(!mysql_query($command,$con)){
-			echo "gg deisga!";
+		$result=mysql_query("SELECT * FROM User WHERE UserName='$UserName' ",$con);
+		$row=mysql_fetch_row($result);
+		if(!$row){
+			$command="INSERT INTO User(UserName,Password,RealName,Email,QQ,Hometown,Signature)
+		                                                    VALUES('$UserName','$Password','$RealName','$Email','$QQ','$Hometown','$Signature' )";
+			if(!mysql_query($command,$con)){
+				die("Failed to sign up!");
+			}else{
+				echo "<script language='JavaScript'>alert('Sign up successfully!')</script>";
+			}
+		}else{
+			echo "<script language='JavaScript'>alert('User exits already!')</script>";
 		}
-		echo "<script language='JavaScript'>alert('Sign up successfully!')</script>";
-		echo "<body>
-				<div class='header'>
-					<h1>Welcome to GuestBook</h1>
-				</div>
-
-				<div style='text-align:center;height:755px;padding:10px;font-size:17px;'>
-					<h2 style='text-align:center'>Sign in to GuestBook</h2><br/>
-		
-				</div>
-	
-				<div class='footer'>
-					<h3>Powered By Zewei Zhang</h3>
-				</div>
-			  </body>";
-                die();
 	}else{
 		echo "<script language='JavaScript'>alert('Item with * is required!')</script>";
 	}
@@ -92,19 +84,20 @@ function customError($errno, $errstr)
 
 	<div style='text-align:center;height:762px;padding:10px;font-size:17px;'>
 		<h2>Sign up to GuestBook</h2><br/>
-		<form action="<?php echo $_SERVER['PHP_SELF'];?>" method='post'>
+		<form accept-charset="UTF-8" action="<?php echo $_SERVER['PHP_SELF'];?>" method='post'>
 				<table style='margin:0 auto;border-width:0px;' border='1' cellspacing='0' cellpadding='5px;'>
 					<tr>
-						<td><span>*</span>Username:</td><td colspan='3'><input type='text' name='UserName'></td>
+						<td><span>*</span>Username:</td><td colspan='3'><input type='text' name='UserName' value='<?php echo $UserName; ?>'></td>
+					</tr>
+					<tr><td><span>*</span>Password:</td><td colspan='3'><input type='password' name='Password'></td></tr>
+					<tr>
+						<td><span>*</span>Name:</td><td><input type='text' name='Name' value='<?php echo $RealName;?>'></td><td>QQ:</td><td><input type='text' name='QQ' value='<?php echo $QQ;?>'></td>
 					</tr>
 					<tr>
-						<td><span>*</span>Name:</td><td><input type='text' name='Name'></td><td>QQ:</td><td><input type='text' name='QQ'></td>
+						<td>Email:</td><td><input type='text' name='Email' value='<?php echo $Email;?>'></td><td>From:</td><td><input type='text' name='Hometown' value='<?php echo $Hometown;?>'></td>
 					</tr>
 					<tr>
-						<td>Email:</td><td><input type='text' name='Email'></td><td>From:</td><td><input type='text' name='Hometown'></td>
-					</tr>
-					<tr>
-						<td><span>*</span>Signature:</td><td><textarea style='font-size:17px;' name='Signature' rows='6' cols='25'></textarea></td><td>Head img:</td><td><input type='radio' name='Himg' checked><input type='radio' name='Himg'></td>
+						<td><span>*</span>Signature:</td><td><textarea style='font-size:17px;' name='Signature' rows='6' cols='25'><?php echo $Signature; ?></textarea></td><td>Head img:</td><td><input type='radio' name='Himg' checked><input type='radio' name='Himg'></td>
 					</tr>
 					<tr>
 						<td style='font-size:0px;padding:2' colspan='4'>&nbsp;</td>
@@ -114,6 +107,7 @@ function customError($errno, $errstr)
 					</tr>
 				</table>
 		</form>
+		<h3><a style='text-decoration:none;color:#0366d6;' href='index.php'>Click me to sign in</a></h3>
 	</div>
 	
 	<div class='footer'>
